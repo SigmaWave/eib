ad6303100724f50cc73e5c039e8432263e02807d
Pierre P <95609842+SigmaWave@users.noreply.github.com>
2026-07-31 15:39:06 +0200

    fix vm_setup

--- vm_setup.sh
+++ vm_setup.sh
@@ -3,17 +3,12 @@ set -e
 
 # System dependencies
 sudo apt-get update
-sudo apt-get install -y python3-pip python3-venv git
+sudo apt-get install -y python3-pip
 
 # Clone repo
 git clone https://github.com/SigmaWave/eib.git
 cd eib
 mkdir -p database
-
-# Python environment
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
 pip install -r requirements.txt
 
 # Ollama + model
